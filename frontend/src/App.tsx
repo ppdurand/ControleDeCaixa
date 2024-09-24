498785a32c1f8a337373ec7f667674298f1a7a40
Pedro Paulo Durand <pedropaulodurand@gmail.com>
2024-09-24 11:16:54 -0300

    Initial frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import { Dashboard } from "./pages/dashboard/Dashboard";
+
+function App() {
+  return (
+    <div className="App">
+      <Dashboard />
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/pages/dashboard/Dashboard.tsx
@@ -0,0 +1,7 @@
+export const Dashboard = () => {
+    return(
+        <div>
+            <h1>Controle de Caixa</h1>
+        </div>
+    );
+}
