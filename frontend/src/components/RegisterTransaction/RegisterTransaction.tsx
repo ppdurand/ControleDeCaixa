6668cd788095ff448a4006e8a6c667b4e23d8cbe
Pedro Paulo Durand <pedropaulodurand@gmail.com>
2024-11-04 11:13:52 -0300

    fix: fieldset

--- frontend/src/components/RegisterTransaction/RegisterTransaction.tsx
+++ frontend/src/components/RegisterTransaction/RegisterTransaction.tsx
@@ -1,14 +1,15 @@
 import { useForm } from 'react-hook-form';
-import { Button, Form, Input, DatePicker, InputNumber, DatePickerProps, Space, Radio } from 'antd';
-import './RegisterTransaction.css';
+import { Button, Form, Input, DatePicker, InputNumber, DatePickerProps, Space, Radio, Row, Col } from 'antd';
+// import './RegisterTransaction.css';
 import 'react-toastify/dist/ReactToastify.css';
 
-export const RegisterTransaction = (props: {addTransaction: any}) => {
+export const RegisterTransaction = (props: { addTransaction: any }) => {
     const { register, handleSubmit, formState: { errors }, reset } = useForm();
     const today = new Date().toISOString().split("T")[0];
 
     const onSubmit = async (data: any) => {
         try {
+            console.log(data)
             props.addTransaction(data);
             reset();
         } catch (error) {
@@ -16,34 +17,47 @@ export const RegisterTransaction = (props: {addTransaction: any}) => {
     }
     const onChange: DatePickerProps['onChange'] = (date, dateString) => {
         console.log(date, dateString);
-      };
+    };
 
     return (
         <div className="registerComponent">
             <h2 className="título">Movimentações</h2>
-            <Form onFinish={handleSubmit(onSubmit)} className='formulario'>
-                <fieldset></fieldset>
-                <Form.Item label="Valor" className='value'>
-                    <InputNumber min={1} placeholder='Digite um valor'/>
-                </Form.Item>
-                <Form.Item label="Data">
-                    <Space direction="vertical">
-                        <DatePicker onChange={onChange} placeholder='Informe uma data' />
-                    </Space>
-                </Form.Item>   
-                <Form.Item label="Observação">
+            <Form layout='vertical' onFinish={onSubmit}
+            >
+                <Row gutter={10}>
+                    <Col span={12}>
+                        <Form.Item
+                            name='value'
+                            label="Valor"
+                            rules={[{ required: true, message: 'O valor é obrigatório' }]}>
+                            <InputNumber min={1} placeholder='Digite um valor' />
+                        </Form.Item>
+                    </Col>
+                    <Col span={12}>
+                        <Form.Item name='date' label="Data">
+                            <Space direction="vertical">
+                                <DatePicker placeholder='Informe uma data' />
+                            </Space>
+                        </Form.Item>
+                    </Col>
+                </Row>
+
+                <Form.Item name='observation' label="Observação">
                     <Input placeholder='Digite uma observação'></Input>
                 </Form.Item>
-                <Form.Item label="Tipo de Movimentação">
+                <Form.Item name='type' label="Tipo de Movimentação">
                     <Radio.Group>
                         <Space direction="horizontal">
-                            <Radio value={1}>Receita</Radio>
-                            <Radio value={2}>Despesa</Radio>
+                            <Radio value='RECEITA'>Receita</Radio>
+                            <Radio value='DESPESA'>Despesa</Radio>
                         </Space>
                     </Radio.Group>
                 </Form.Item>
+                <Row>
+                    <Button type="primary" htmlType='submit'>Enviar</Button>
+                </Row>
             </Form>
-            <Button type="primary">Enviar</Button>
+
         </div>
     )
 }
